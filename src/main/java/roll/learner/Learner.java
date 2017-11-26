da3d7a3eb542e20079201cc19a252a82c871e8bd
duckly <duckly_lee@163.com>
2017-11-26 21:54:41 +0800

    update headers

--- src/main/java/roll/learner/Learner.java
+++ src/main/java/roll/learner/Learner.java
@@ -1,17 +1,18 @@
-/* Written by Yong Li, Depeng Liu                                       */
-/* Copyright (c) 2016                  	                               */
-/* This program is free software: you can redistribute it and/or modify */
-/* it under the terms of the GNU General Public License as published by */
-/* the Free Software Foundation, either version 3 of the License, or    */
-/* (at your option) any later version.                                  */
+/* Copyright (c) 2016, 2017                                               */
+/*       Institute of Software, Chinese Academy of Sciences               */
+/* This file is part of ROLL, a Regular Omega Language Learning library.  */
+/* ROLL is free software: you can redistribute it and/or modify           */
+/* it under the terms of the GNU General Public License as published by   */
+/* the Free Software Foundation, either version 3 of the License, or      */
+/* (at your option) any later version.                                    */
 
-/* This program is distributed in the hope that it will be useful,      */
-/* but WITHOUT ANY WARRANTY; without even the implied warranty of       */
-/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        */
-/* GNU General Public License for more details.                         */
+/* This program is distributed in the hope that it will be useful,        */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of         */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          */
+/* GNU General Public License for more details.                           */
 
-/* You should have received a copy of the GNU General Public License    */
-/* along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+/* You should have received a copy of the GNU General Public License      */
+/* along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 package roll.learner;
 
@@ -20,6 +21,8 @@ import roll.query.Query;
 /**
  * @M the output automata hypothesis 
  * @O output type
+ * 
+ * @author Yong Li (liyong@ios.ac.cn)
  * */
 public interface Learner<M, O> {
 	
