c89035ba00a0ce526f24e4a34b99bf3cdce3bd7d
Andrea Turrini <andrea.turrini@gmail.com>
2018-11-17 17:25:29 +0800

    Fixed management of spaces in directories in build-offine.sh

--- build-offline.sh
+++ build-offline.sh
@@ -8,29 +8,29 @@ fi
 BASEDIR=`pwd`
 mkdir -p ROLL
 
-cd $BASEDIR/src/main/javacc/roll/parser/ba/
-java -cp $BASEDIR/javacc.jar javacc JBAParser.jj
+cd "$BASEDIR"/src/main/javacc/roll/parser/ba/
+java -cp "$BASEDIR"/javacc.jar javacc JBAParser.jj
 
-cd $BASEDIR/src/main/java
+cd "$BASEDIR"/src/main/java
 JF=""
 for i in `find . -name '*.java'` `find ../javacc/ -name '*.java'`; do JF="$JF $i"; done
 
 CP=".:$BASEDIR/ROLL/"
-for i in $BASEDIR/lib/*jar; do 
+for i in "$BASEDIR"/lib/*jar; do 
 	CP=$CP:$i
 done
 
-javac -cp $CP -d $BASEDIR/ROLL/ $JF
+javac -cp "$CP" -d "$BASEDIR"/ROLL/ $JF
 
-cd $BASEDIR/ROLL
-for j in $BASEDIR/lib/*.jar; do
+cd "$BASEDIR"/ROLL
+for j in "$BASEDIR"/lib/*.jar; do
 	jar xf "$j"; 
 done
 
 mkdir -p META-INF
 echo "Manifest-Version: 1.0" > META-INF/MANIFEST.MF
 echo "Main-Class: roll.main.ROLL" >> META-INF/MANIFEST.MF
-jar cfm $BASEDIR/ROLL.jar META-INF/MANIFEST.MF .
+jar cfm "$BASEDIR"/ROLL.jar META-INF/MANIFEST.MF .
 
-cd $BASEDIR
+cd "$BASEDIR"
 rm -r ROLL
