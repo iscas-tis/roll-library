d87a2350d63bd4819e0352d1a07daeb35a4a320c
liyong <liyong@DESKTOP-R79036K>
2021-04-19 02:21:56 +0800

    add abstract class complement

---
+++ src/main/java/roll/main/complement/Complement.java
@@ -0,0 +1,21 @@
+package roll.main.complement;
+
+import roll.automata.NBA;
+
+public abstract class Complement extends NBA {
+	
+    protected final NBA operand;
+
+	public Complement(NBA operand) {
+		super(operand.getAlphabet());
+		this.operand = operand;
+		computeInitialState();
+	}
+	
+    protected abstract void computeInitialState();
+	
+	public NBA getOperand() {
+		return this.operand;
+	}
+
+}
