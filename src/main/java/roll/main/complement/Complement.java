571cf70680ea522bd2246610e1ff8e1c29801d11
Yong Li <duckly_lee@163.com>
2021-04-23 20:37:54 +0800

    add minimization for progress DFAs

--- src/main/java/roll/main/complement/Complement.java
+++ src/main/java/roll/main/complement/Complement.java
@@ -1,16 +1,23 @@
 package roll.main.complement;
 
 import roll.automata.NBA;
+import roll.main.Options;
 
 public abstract class Complement extends NBA {
 	
     protected final NBA operand;
+	protected Options options;
 
-	public Complement(NBA operand) {
+	public Complement(Options options, NBA operand) {
 		super(operand.getAlphabet());
 		this.operand = operand;
+		this.options = options;
 		computeInitialState();
 	}
+	
+    public Options getOptions() {
+    	return this.options;
+    }
 	
     protected abstract void computeInitialState();
 	
